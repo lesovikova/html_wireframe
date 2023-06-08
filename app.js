4ac4617f1653e7cbbf1c36e60b156ce93ea30ff8
__lesovikova__ <__lesovikova@gmail.com__>
2023-06-08 14:43:58 +0100

    add function to change colors on page on click]

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const heading = document.getElementById("heading");
+const changeButton = document.querySelector('.button__item');
+
+const headerLinks = document.querySelectorAll(".header__menu-link");
+const headers2 = document.querySelectorAll("h2");
+const headers3 = document.querySelectorAll("h3");
+const footerLinks = document.querySelectorAll(".footer__link");
+
+const nodesArray = [heading, changeButton, ...headerLinks, ...headers2, ...headers3, ...footerLinks];
+
 
 let userName = prompt("What is your user name");
+
 compareName();
 
+// Changes heading depending on what user puts in a prompt
 function compareName(){
 if(userName === "Admin") {
     heading.textContent = "Hello Admin"; 
@@ -11,3 +22,24 @@ if(userName === "Admin") {
 }
 }
 
+// Creates random number from 0 to 255;
+function getRandomNumber() {
+    return Math.floor(Math.random() * 256);
+}
+
+// creates random number in RGB format
+function getRandomRGB(number) {
+    let RGB = [];
+    for(let i=0; i<3; i++) {
+RGB.push(number());
+    }
+    return RGB.toString();
+}
+
+// on click changes all colors on the page to random
+changeButton.addEventListener("click", (e) => {
+    nodesArray.forEach((item) => {
+        item.style.color = `rgb(${getRandomRGB(getRandomNumber)})`;
+    });
+    changeButton.style.backgroundColor = `rgb(${getRandomRGB(getRandomNumber)})`;
+});
